165d45be96c5e086f532ba5178e8358579a3584a
Rubio <paulocezarrubio@gmail.com>
2022-07-18 19:28:53 -0300

    req 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -20,4 +20,29 @@ describe('Testa o componente Pokemon', () => {
     expect(screen.getByAltText(/pikachu sprite/i).src)
       .toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
+  test('Se o card renderizado do pokemon possue um link para detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(
+      screen.getByRole('link', { name: /more details/i }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /more details/i }));
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+    expect(history.location.pathname).toBe('/pokemons/25');
+  });
+  test('Se o pokemon possue um icone de estrela se ele for favoritado', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+
+    const favorite = screen.getByText(/Pokémon favoritado?/i);
+    userEvent.click(favorite);
+
+    expect(
+      screen.getByAltText(/Pikachu is marked as favorite/i),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByAltText(/Pikachu is marked as favorite/i).src,
+    ).toEqual(expect.stringContaining('/star-icon.svg'));
+  });
 });
