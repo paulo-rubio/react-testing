79bad03e3d08f16c542af30f9e4bb7bb632409e7
Rubio <paulocezarrubio@gmail.com>
2022-07-18 18:48:04 -0300

    uma parte do req6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// import pokemons from '../data';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente Pokemon', () => {
+  test('Se é renderizado um card com as informações do pokemon', () => {
+    renderWithRouter(<App />);
+
+    expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent(/Pikachu/i);
+    expect(screen.getByTestId('pokemon-type')).toBeInTheDocument();
+    expect(screen.getByTestId('pokemon-type')).toHaveTextContent(/Electric/i);
+    expect(screen.getByTestId('pokemon-weight')).toBeInTheDocument();
+    expect(screen.getByTestId('pokemon-weight'))
+      .toHaveTextContent(/Average weight: 6.0 kg/i);
+    expect(screen.getByAltText(/pikachu sprite/i)).toBeInTheDocument();
+    expect(screen.getByAltText(/pikachu sprite/i).src)
+      .toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+});
