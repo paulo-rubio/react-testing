9f7bf1ab90d72df8f23a867cbf2731ebc7e33efb
Rubio <paulocezarrubio@gmail.com>
2022-07-18 17:18:43 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente NotFound', () => {
+  test('se a pagina recebe um h2  de Page requested not found 😭', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/....');
+
+    expect(
+      screen.getByRole('heading', { name: /Not Found/i, level: 2 }),
+    ).toBeInTheDocument();
+  });
+  test('se a pagina recebe um gif', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/....');
+
+    expect(
+      screen.getByRole('img', { name: /not Found/i }).src,
+    ).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
