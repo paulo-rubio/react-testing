a338ecb05d8918b60165d6cb980714408d0f18b6
Rubio <paulocezarrubio@gmail.com>
2022-07-18 16:23:00 -0300

    req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,49 @@
-test('1', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+// import { Pokedex } from '../pages';
+
+describe('Testa o componente About', () => {
+  test('Testa se a pagina contem informações da pokedex', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const aboutHome = screen.getByRole('heading', { name: /Pokédex/i, level: 1 });
+    expect(aboutHome).toBeDefined();
+  });
+  test('Testa se a pagina contem um h2 com o texto About Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+
+    expect(
+      screen.getByRole('heading', { name: /About Pokédex/i, level: 2 }),
+    );
+  });
+  test('Testa se a pagina contem dois paragrafos com textos sobre a Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+
+    expect(
+      screen.getByText(
+        /This application simulates a Pokédex/i,
+      ),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        /One can filter Pokémons by type, and see more/i,
+      ),
+    ).toBeInTheDocument();
+  });
+  test('Testa se aparece uma imagem de Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+
+    expect(
+      screen.getByRole('img', { name: /Pokédex/i }).src,
+    ).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,61 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+// import renderWithRouter from './renderWithRouter';
+
+describe('Testa os componentes do app.js', () => {
+  test('Testa se o link home existe e deve te redirecionar a Home', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const homePage = screen.getByRole('heading', { name: /Pokédex/i });
+    expect(homePage).toBeDefined();
+
+    const getLinkHome = screen.getByRole('link', { name: /home/i });
+    expect(getLinkHome).toBeDefined();
+
+    userEvent.click(getLinkHome);
+
+    const headingHome = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    expect(headingHome).toHaveTextContent(/Encountered pokémons/i);
+  });
+  test('Testa se o link home existe e deve te redirecionar a About', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const aboutPage = screen.getByRole('heading', { name: /Pokédex/i });
+    expect(aboutPage).toBeDefined();
+
+    const getLinkAbout = screen.getByRole('link', { name: /about/i });
+    expect(getLinkAbout).toBeDefined();
+
+    userEvent.click(getLinkAbout);
+
+    const headingHome = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(headingHome).toHaveTextContent(/About Pokédex/i);
+  });
+  test('Testa se o link home existe e deve te redirecionar a Favorite Pokédex', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const pokedexPage = screen.getByRole('heading', { name: /Pokédex/i });
+    expect(pokedexPage).toBeDefined();
+
+    const getLinkPokedex = screen.getByRole('link', { name: /Favorite pokémons/i });
+    expect(getLinkPokedex).toBeDefined();
+
+    userEvent.click(getLinkPokedex);
+
+    const headingHome = screen.getByRole('heading', { name: /Favorite pokémons/i });
+    expect(headingHome).toHaveTextContent(/Favorite pokémons/i);
+  });
+});
